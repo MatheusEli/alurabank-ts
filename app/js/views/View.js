1852b8f8d86efa1f2886bd720c96a74b4dc9b8f8
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-01-04 10:20:24 -0300

    commit inicial

---
+++ app/js/views/View.js
@@ -0,0 +1,11 @@
+class View {
+    constructor(seletor) {
+        this._elemento = document.querySelector(seletor);
+    }
+    update(model) {
+        this._elemento.innerHTML = this.template(model);
+    }
+    template(model) {
+        throw new Error("Você deve implementar o método TEMPLATE");
+    }
+}
---
+++ app/ts/views/View.ts
@@ -0,0 +1,19 @@
+class View<T>{
+    
+    protected _elemento:Element;
+
+    constructor(seletor:string){
+
+        this._elemento = document.querySelector(seletor);
+    }
+
+    update(model:T):void{
+
+        this._elemento.innerHTML = this.template(model);
+    }
+
+    template(model:T):string{
+
+        throw new Error("Você deve implementar o método TEMPLATE");
+    }
+}
